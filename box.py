87c611b7f7b60c3a1d479a29c9a812a9f32ebc4b
Vera Nemedy-Varga <n.v.vera@gmail.com>
2017-04-10 10:08:37 +0200

    constructor and get_surface method created

---
+++ box.py
@@ -0,0 +1,17 @@
+class Cuboid(object):
+    def __init__(self, width, height, depth):
+        self.width = width
+        self.height = height
+        self.depth = depth
+
+    def get_surface(self):
+        self.surface = 2*(self.width*self.height + self.width*self.depth + self.height*self.depth)
+        return self.surface
+    # Create a class that represents a cuboid:
+    # It should take its three dimensions as constructor parameters (numbers)
+    # It should have a method called `get_surface` that returns the cuboid's surface
+    # It should have a method called `get_volume` that returns the cuboid's volume
+
+box = Cuboid(10, 20, 30)
+print(box.get_surface()) # should print 2200
+#print(box.get_volume()) # should print 6000
